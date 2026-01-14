30396f459019691d3f786eb30066745c88b050f0
venkat-wang <venkat@yeah.net>
2026-01-14 21:28:47 +0800

    'deploy1'

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/vue-admin-web/'
+    ? '/score-keeper/'
     : '/',
   plugins: [
     vue(),
