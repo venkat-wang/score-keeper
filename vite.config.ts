c74a6b951e495b83aa66dbb695bab8c0d79c9b06
venkat-wang <venkat@yeah.net>
2026-01-14 21:26:44 +0800

    'deploy'

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/vue-admin-web/'
+    : '/',
   plugins: [
     vue(),
     vueJsx(),
